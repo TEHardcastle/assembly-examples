7e724212999b74c33eab8ff4b35b4a5a0f4c6a44
Tom Hardcastle <tomhardcastle@hotmail.co.uk>
2021-10-09 13:16:51 +0100

    added compile script, src directory, and fibonacci readme file

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir bin
+mkdir obj
+
+for folder in src/*
+do
+	for FILE in $folder/*
+	do
+		file_ext=$(echo $FILE | cut -d "." -f "2")
+		file_root=$(echo $FILE | cut -d "." -f "1" | cut -d "/" -f "3")
+		if [ $file_ext = "s" ]
+		then
+			as -ggdb $FILE -o obj/$file_root.o
+			ld obj/$file_root.o -o bin/$file_root
+		fi
+	done
+done
